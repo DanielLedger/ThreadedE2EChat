9dceb16f1734a06da0496f8e01ea7277570d2799
BuildTools <72450790+DanielLedger@users.noreply.github.com>
2020-10-28 18:09:48 +0000

    Bugs fixed.
    Main bug being HELLO packets not having a sender (which is objectively false).

--- ThreadedChat-Server/src/me/DanL/ThreadedServer/PacketManage/PacketParser.java
+++ ThreadedChat-Server/src/me/DanL/ThreadedServer/PacketManage/PacketParser.java
@@ -3,12 +3,15 @@ package me.DanL.ThreadedServer.PacketManage;
 import java.util.Base64;
 import java.util.UUID;
 
+import me.DanL.ThreadedServer.Primary.Server;
+
 /**
  * A packet parser that parses all incoming packets into a usable object.
  * @author daniel
  *
  */
 public class PacketParser {
+	
 	public enum PacketType{
 		HELLO,
 		PERSON,
@@ -20,9 +23,9 @@ public class PacketParser {
 		SEND
 	}
 	
-	private String payload;
+	private String payload = "";
 	
-	private UUID sender;
+	private UUID sender = null;
 	
 	private int packetNum;
 	
@@ -58,56 +61,112 @@ public class PacketParser {
 	}
 	
 	public PacketParser(String packetData) {
-		String[] parts = packetData.split(packetData);
+		String[] parts = packetData.trim().split(" ");
 		String type = parts[0];
-		typeOf = PacketType.valueOf(type);
-		if (typeOf == null) {
+		try {
+			typeOf = PacketType.valueOf(type);
+		}
+		catch (IllegalArgumentException e) {
+			return; //Invalid packet type.
+		}
+		try {
+			switch (typeOf) {
+			case CRYPT:
+				payload = parts[1];
+				break;
+			case GET:
+				payload = parts[1];
+				packetNum = Integer.parseInt(parts[2]);
+				authToken = Base64.getDecoder().decode(parts[3]);
+				sender = UUID.fromString(parts[4]);
+				break;
+			case GETID:
+				payload = parts[1];
+				packetNum = Integer.parseInt(parts[2]);
+				authToken = Base64.getDecoder().decode(parts[3]);
+				sender = UUID.fromString(parts[4]);
+				break;
+			case HELLO:
+				sender = UUID.fromString(parts[1]);
+				break;
+			case KEY:
+				payload = parts[1];
+				packetNum = Integer.parseInt(parts[2]);
+				authToken = Base64.getDecoder().decode(parts[3]);
+				sender = UUID.fromString(parts[4]);
+				break;
+			case MESSAGES:
+				packetNum = Integer.parseInt(parts[1]);
+				authToken = Base64.getDecoder().decode(parts[2]);
+				sender = UUID.fromString(parts[3]);
+				break;
+			case PERSON:
+				payload = parts[1];
+				break;
+			case SEND:
+				payload = parts[1] + " " + parts[2];
+				packetNum = Integer.parseInt(parts[3]);
+				authToken = Base64.getDecoder().decode(parts[4]);
+				sender = UUID.fromString(parts[5]);
+				break;
+			}
+		}
+		catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+			//Failed to parse packet correctly, so just return.
+			e.printStackTrace();
 			return;
 		}
-		switch (typeOf) {
-		case CRYPT:
-			payload = parts[1];
-			break;
-		case GET:
-			payload = parts[1];
-			packetNum = Integer.parseInt(parts[2]);
-			authToken = Base64.getDecoder().decode(parts[3]);
-			sender = UUID.fromString(parts[4]);
-			break;
-		case GETID:
-			payload = parts[1];
-			packetNum = Integer.parseInt(parts[2]);
-			authToken = Base64.getDecoder().decode(parts[3]);
-			sender = UUID.fromString(parts[4]);
-			break;
-		case HELLO:
-			payload = parts[1];
-			break;
-		case KEY:
-			payload = parts[1];
-			packetNum = Integer.parseInt(parts[2]);
-			authToken = Base64.getDecoder().decode(parts[3]);
-			sender = UUID.fromString(parts[4]);
-			break;
-		case MESSAGES:
-			packetNum = Integer.parseInt(parts[1]);
-			authToken = Base64.getDecoder().decode(parts[2]);
-			sender = UUID.fromString(parts[3]);
-			break;
-		case PERSON:
-			payload = parts[1];
-			break;
-		case SEND:
-			payload = parts[1] + " " + parts[2];
-			packetNum = Integer.parseInt(parts[3]);
-			authToken = Base64.getDecoder().decode(parts[4]);
-			sender = UUID.fromString(parts[5]);
-			break;
-		}
+		valid = true; //Packet has the required structure
+		authenticated = (!requiresAuth(typeOf)) || Server.getAuthProvider().packetAuthed(payload, sender, packetNum, authToken);
 	}
 	
+	public UUID getSender() {
+		return sender;
+	}
+
 	@Override
 	public String toString() {
-		return "";
+		String desc = "";
+		if (!valid) {
+			return "Invalid packet!\n";
+		}
+		else if (!authenticated) {
+			desc = "Unauthenticated packet:\n";
+		}
+		else {
+			desc = "Authenticated packet:\n";
+		}
+		desc += "Packet type: " + typeOf.toString();
+		if (sender != null) {
+			desc += "\nSender ID: " + sender.toString();
+		}
+		else {
+			desc += "\nNo declared sender.";
+		}
+		desc += "\nPacket payload: " + payload;
+		desc += "\n";
+		return desc;
+	}
+
+	/**
+	 * @return the valid
+	 */
+	public boolean isValid() {
+		return valid;
+	}
+
+	/**
+	 * @return the authenticated
+	 */
+	public boolean isAuthenticated() {
+		return authenticated;
+	}
+	
+	public PacketType getType() {
+		return typeOf;
+	}
+	
+	public String payload() {
+		return payload;
 	}
 }
