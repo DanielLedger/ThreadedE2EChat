6049f4b8eb90cc951eb04c4bec13a9711c4ce96f
BuildTools <72450790+DanielLedger@users.noreply.github.com>
2020-10-28 18:09:29 +0000

    Updated main for other changes.

--- ThreadedChat-Server/src/me/DanL/ThreadedServer/Primary/Main.java
+++ ThreadedChat-Server/src/me/DanL/ThreadedServer/Primary/Main.java
@@ -1,16 +1,20 @@
 package me.DanL.ThreadedServer.Primary;
 
+import java.io.File;
 import java.io.IOException;
 
 import me.DanL.PacketManager.Connection;
 import me.DanL.ThreadedServer.PacketManage.MasterPacketHandler;
+import me.DanL.ThreadedServer.UserManagement.Authenticator;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		MasterPacketHandler mph = new MasterPacketHandler();
+		File keyStorageDirectory = new File("crypt/keys");
+		Server.setAuthProvider(new Authenticator(keyStorageDirectory));
 		while (true) {
-			Connection.onRecv(4444, 2048, mph);
+			Connection.onRecv(4444, 65535, mph);
 		}
 	}
 
